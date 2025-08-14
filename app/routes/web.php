d93c368e81a542f95c18a19dc69288f1cc5771d0
NahoUchiyama <naho.violin0620@gmail.com>
2025-08-14 21:45:50 +0900

    2025/8/14-新規登録実装中

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\DisplayController;
+use App\Http\Controllers\Auth\LoginController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,4 +22,16 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/users/top', [DisplayController::class, 'index']);
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/login', function () {
+    return view('auth.login'); 
+});
+
+
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
